f253f5cc2e35c3bbdc0cee9632e9149c1e06f080
MJ van Staden <mjvstaden01@gmail.com>
2024-05-14 22:04:10 +0200

    Register frontend and backend integration

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client";
 
-type RegisterFormData = {
+export type RegisterFormData = {
   firstName: string;
   lastName: string;
   email: string;
@@ -11,8 +13,16 @@ type RegisterFormData = {
 const Register = () => {
 
   const { register, watch, handleSubmit, formState: {errors}, } = useForm<RegisterFormData>();
+  const mutation = useMutation(apiClient.register, {
+    onSuccess: () => {
+      alert("Account created successfully");
+    },
+    onError: (error: Error) => {
+      alert(error.message);
+    }
+  });
   const onSubmit = handleSubmit((data) => {
-    console.log(data);
+    mutation.mutate(data);
   });
 
     return (
